e3c34b3b1f8b8545050a14dfa7ce72a93b2bf0d9
GoncaloAlmeida <goncalo.almeida88@gmail.com>
2025-03-18 18:54:04 +0000

    Initial Commit

---
+++ ReceiptSnap/ReceiptSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptSnapApp.swift
+//  ReceiptSnap
+//
+//  Created by Gonçalo Almeida on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
